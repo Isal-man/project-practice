d78a19ab8367c5e285ef368bfaf2474a6a58217c
isal-man <faiskweren7@gmail.com>
2023-08-14 17:37:48 +0700

    Spring Boot with Microsoft Azure (App service and Database)

---
+++ src/main/java/com/access/projectpractice/Employee.java
@@ -0,0 +1,19 @@
+package com.access.projectpractice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String dept;
+    private Long salary;
+
+}
